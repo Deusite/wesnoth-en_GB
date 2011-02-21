79cbbd0c4e4c25c8ae2b4eccfde501dda2d33827
Espreon <Majora700@gmail.com>
2011-02-20 20:27:15 -0500

    Revert recent changes to script. I shall make the source and destination directories specifiable... later.

--- utils/update-po-files.sh
+++ utils/update-po-files.sh
@@ -38,6 +38,5 @@ i=0
 while [ "$i" -lt "$E" ]
 do
 let "i++"
-cp ~/wesnoth-1.8/po/${td[$i]}/en_GB.po 
-~/wesnoth-en_GB/${td[$i]}/en_GB.po
+cp ~/wesnoth/po/${td[$i]}/en_GB.po ~/wesnoth-en_GB/${td[$i]}/en_GB.po
 done
